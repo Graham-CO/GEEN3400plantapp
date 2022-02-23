4a5ba493c8ff4bd1a672f5793b3211575ec14c28
Emily Harrison <ehemilyharrison@gmail.com>
2022-02-23 10:14:47 -0700

    getUserbyID

--- datalayer/controllers/user/user.go
+++ datalayer/controllers/user/user.go
@@ -13,3 +13,16 @@ import (
 func GetUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
 }
+
+func GetUserByID(c *gin.Context){
+	userid:= c.Param("userid")
+
+	//Loop over users to find album whose ID matches parameter
+	for _, a := range users{
+		if a.UserID == userid {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
