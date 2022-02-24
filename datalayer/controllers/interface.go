e7f02ff066f01a0519c8efdd5b7382e42a959dfb
Emily Harrison <ehemilyharrison@gmail.com>
2022-02-23 23:09:26 -0700

    pot.go controllers

--- datalayer/controllers/interface.go
+++ datalayer/controllers/interface.go
@@ -11,9 +11,14 @@ type PlantDef interface {
 }
 
 type Pot interface {
+	GetPots(c *gin.Context)
+	GetPotByID(c *gin.Context)
+	AddPot(c *gin.Context)
 	GetTemp(c *gin.Context)
 }
 
 type User interface {
 	GetUsers(c *gin.Context)
+	GetUserByID(c *gin.Context)
+	CreateUser(c *gin.Context)
 }
