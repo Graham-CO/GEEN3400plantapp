ea9bffe87d99ea9d2c0f390c7d9dfee53a578ad0
Emily Harrison <ehemilyharrison@gmail.com>
2022-02-23 23:03:47 -0700

    capitalizing getUsers

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/users", getUsers) /*use get to associate GET HTTP and /albums path with handler function.
+	router.GET("/users", GetUsers) /*use get to associate GET HTTP and /albums path with handler function.
 	passes the NAME of the function, not the result, which you would do by typing getUsers()  */
-	router.GET("/users/:userid", getUserByID)
-	router.POST("/users", postUsers)
+	router.GET("/users/:userid", GetUserByID)
+	router.POST("/users", CreateUser)
 
 	router.Run("localhost:8080") //starts the server
 }
