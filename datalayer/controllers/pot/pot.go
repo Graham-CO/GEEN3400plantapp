d69fed9e44ec93df307ba8203172778757752eca
Emily Harrison <ehemilyharrison@gmail.com>
2022-02-24 13:43:18 -0700

    GetTemp draft

--- datalayer/controllers/pot/pot.go
+++ datalayer/controllers/pot/pot.go
@@ -9,16 +9,18 @@ import (
 // RESTful C.R.U API controller functions
 // e.g., GET, POST, etc.
 
+//Get list of all pots
 func GetPots(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, pots)
 }
 
+//pull out a pot by its ID
 func GetPotByID(c *gin.Context) {
 	potid := c.Param("potid")
 
-	//Loop over users to find album whose ID matches parameter
+	//Loop over pots to find pot whose ID matches parameter
 	for _, a := range pots {
-		if a.UserID == potid {
+		if a.PotID == potid {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
@@ -36,10 +38,22 @@ func AddPot(c *gin.Context) {
 
 	//Add new pot to the slice
 	users = append(pots, newPot)
-	c.IndentedJSON(http.StatusCreated, newUser)
+	c.IndentedJSON(http.StatusCreated, newPot)
 }
 
+// TODO: Check if this actually works! How to retrieve specific value from JSON?
 // getTemp responds with the float64 temp sens value
 func GetTemp(c *gin.Context) {
-	// TODO: How to retrieve specific value from JSON?
+	potid := c.Param("potid")
+	temp := c.Param("temp")
+
+	//Loop over pots to find pot whose ID matches parameter
+	for _, a := range pots {
+		if a.PotID == potid {
+			b := a.Temp
+			c.IndentedJSON(http.StatusOK, b)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "pot not found"})
 }
