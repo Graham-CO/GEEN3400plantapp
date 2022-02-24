28190d68f1366d4dd875faa726bbb284ec4948c1
Emily Harrison <ehemilyharrison@gmail.com>
2022-02-23 23:02:30 -0700

    addpot, getpots, getpotbyid

--- datalayer/controllers/pot/pot.go
+++ datalayer/controllers/pot/pot.go
@@ -1,9 +1,43 @@
 package pot
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // RESTful C.R.U API controller functions
 // e.g., GET, POST, etc.
+
+func GetPots(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, pots)
+}
+
+func GetPotByID(c *gin.Context) {
+	potid := c.Param("potid")
+
+	//Loop over users to find album whose ID matches parameter
+	for _, a := range pots {
+		if a.UserID == potid {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "pot not found"})
+}
+
+func AddPot(c *gin.Context) {
+	var newPot pot
+
+	//Call BindJSON to bind the received JSON to newPot
+	if err := c.BindJSON(&newPot); err != nil {
+		return
+	}
+
+	//Add new pot to the slice
+	users = append(pots, newPot)
+	c.IndentedJSON(http.StatusCreated, newUser)
+}
 
 // getTemp responds with the float64 temp sens value
 func GetTemp(c *gin.Context) {
