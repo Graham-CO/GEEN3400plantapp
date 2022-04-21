b8fe76a096f5cf4d07b7e21174dc6533d97cf879
Graham-CO <grwi2594@colorado.edu>
2022-04-20 23:53:58 -0600

    Fix firebase/gcloud interaction

--- datalayer/controllers/pot/pot.go
+++ datalayer/controllers/pot/pot.go
@@ -17,7 +17,7 @@ func GetTemp(c *gin.Context) {
 	// TODO: How to retrieve specific value from JSON?
 }
 
-func WriteReading(moisture *firestore.CollectionRef) gin.HandlerFunc {
+func WriteReading(pots *firestore.CollectionRef) gin.HandlerFunc {
 	fn := func(ctx *gin.Context) { // ALWAYS return a (handler) func
 		var pot entities.Pot
 
@@ -29,12 +29,12 @@ func WriteReading(moisture *firestore.CollectionRef) gin.HandlerFunc {
 
 		log.Println("moisture data sent to database", pot)
 
-		wr, err := moisture.NewDoc().Create(ctx, &pot) // create new entry in Pot collection
-		if err != nil {                                // server error
+		wr, err := pots.NewDoc().Create(ctx, &pot) // create new entry in Pot collection
+		if err != nil {                            // server error
 			ctx.JSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "message": err.Error()})
 			return
 		}
-		ctx.JSON(http.StatusOK, gin.H{"created_at": wr.UpdateTime, "reading": 50}) // success statement
+		ctx.JSON(http.StatusOK, gin.H{"created_at": wr.UpdateTime, "water": pot.Water}) // success statement
 	}
 	return fn
 }
--- datalayer/main.go
+++ datalayer/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 	"path"
-	"net/http"
-	"fmt"
 	"potbut/datalayer/controllers/plant"
 	"potbut/datalayer/controllers/pot"
 
@@ -15,18 +14,27 @@ import (
 	"google.golang.org/api/option"
 )
 
-func main() {
+func getFirebaseApp() (*firebase.App, error) {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  IDK INTERNET   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	http.HandleFunc("/", indexHandler)
+	if project, exists := os.LookupEnv("GOOGLE_CLOUD_PROJECT"); exists {
+		ctx := context.Background()
+		conf := &firebase.Config{ProjectID: project}
+		app, err := firebase.NewApp(ctx, conf)
+		return app, err
+	}
+	sa := option.WithCredentialsFile(path.Join(home, "potbut-d8c82-firebase-adminsdk-8rgls-2054b45578.json"))
+	return firebase.NewApp(context.Background(), nil, sa)
+
+}
+
+func main() {
+	app, err := getFirebaseApp()
+
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FIREBASE CLIENT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	//service account
-	sa := option.WithCredentialsFile(path.Join(home, "potbut-d8c82-firebase-adminsdk-8rgls-2054b45578.json"))
-	// register app with Firebase
-	app, err := firebase.NewApp(context.Background(), nil, sa) // context.Background() pushes to worker threads
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
@@ -42,6 +50,7 @@ func main() {
 	router := gin.Default() // default logging and recovery
 	router.POST("/plants", plant.CreatePlant(client.Collection("Plants")))
 	router.POST("/pot", pot.WriteReading(client.Collection("Pots")))
+	router.GET("/", indexHandler)
 
 	port := os.Getenv("PORT")
 
@@ -54,10 +63,6 @@ func main() {
 	router.Run("localhost:" + port) //starts the server, 8080 was being used
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprint(w, "Hello, World!")
+func indexHandler(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "Hello, World!")
 }
